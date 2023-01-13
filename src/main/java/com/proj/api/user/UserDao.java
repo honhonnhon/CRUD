35c19304e44e769ac33e2e1590eea3e211b41d8f
honhonnhon <kornsup@windowslive.com>
2023-01-14 01:22:11 +0700

    update 1.0

--- src/main/java/com/proj/api/user/UserDao.java
+++ src/main/java/com/proj/api/user/UserDao.java
@@ -3,7 +3,7 @@ package com.proj.api.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDao extends JpaRepository {
+public interface UserDao extends JpaRepository<String,User> {
 
     User findByUsername(String username);
 
